496fde8385a3d45e4f36a8cef9f0d390303445ab
Ahmed Hussein <ahmed_hussein.dev@outlook.com>
2018-08-14 23:19:53 +0200

    add basic ant table

--- src/customApp/containers/blankPage.js
+++ src/customApp/containers/blankPage.js
@@ -1,13 +1,39 @@
 import React, { Component } from "react";
 import LayoutContentWrapper from "../../components/utility/layoutWrapper.js";
 import LayoutContent from "../../components/utility/layoutContent";
+import { Table, Icon } from 'antd';
+const dataSource = [{
+  key: '1',
+  name: 'Mike',
+  age: 32,
+  address: '10 Downing Street'
+}, {
+  key: '2',
+  name: 'John',
+  age: 42,
+  address: '10 Downing Street'
+}];
 
+const columns = [{
+  title: 'Name',
+  dataIndex: 'name',
+  key: 'name',
+}, {
+  title: 'Age',
+  dataIndex: 'age',
+  key: 'age',
+}, {
+  title: 'Address',
+  dataIndex: 'address',
+  key: 'address',
+}];
 export default class extends Component {
   render() {
     return (
       <LayoutContentWrapper style={{ height: "100vh" }}>
         <LayoutContent>
           <h1>Blank Page</h1>
+          <Table dataSource={dataSource} columns={columns} />
         </LayoutContent>
       </LayoutContentWrapper>
     );
